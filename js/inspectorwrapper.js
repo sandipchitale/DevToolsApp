7c6ee6a493af6bbdcdaa51e4c61fc21eac88fa18
Sandip Chitale <Sandip.Chitale@emc.com>
2014-12-11 20:07:48 -0800

    CHange URL order.

--- js/inspectorwrapper.js
+++ js/inspectorwrapper.js
@@ -35,7 +35,7 @@
         $scope.config = {
             host: 'localhost',
             port: '9222',
-            devtoolsUrl: 'http://sandipchitaleschromedevtoolsstuff.googlecode.com/git/front_end/inspector.html',
+            devtoolsUrl: 'http://chrome-developer-tools.googlecode.com/git/devtools-frontend/Source/devtools/front_end/inspector.html',
             devtoolsUrls: [
                 'Builtin',
                 'http://src.chromium.org/blink/trunk/Source/devtools/front_end/inspector.html',
